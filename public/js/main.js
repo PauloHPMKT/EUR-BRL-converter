8a46a1149ed12d78352201e554a1872587660bf2
Paulo Sergio <pauloserg861@gmail.com>
2021-11-24 23:07:37 -0300

    adicionando todas as funcionalidades

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,15 @@
 //cashe selectors =============================================================================
 const euroField = document.querySelector('#euroAmount')
-const realField = document.querySelector('#realAmount')
+const priceOfTheDay = document.querySelector('span')
 const btnConverter = document.querySelector('#btnConverter')
+const btnClean = document.querySelector('#btnClean')
+const result = document.querySelector('#result')
 
 //api url Fixer.io ============================================================================
 const urlApiKey = 'http://data.fixer.io/api/latest?access_key=4db85286f1814f557cd74c6881892ffe'
+
+
+//new feature - priceOfTheDay.innerHTML = `Cotação do dia: ${(euroAmount).toFixed(2)}`
 
 //fetch api function ==========================================================================
 function converter() {
@@ -15,11 +20,25 @@ function converter() {
         const euro = euroField.value
         const real = (euro * euroAmount).toFixed(2)
 
-        alert(real)
+        if(euro !== '') {
+            result.innerHTML = `Valor convertido: € ${euro} <br> Valor total: R$ ${real}`
+        } else {
+            result.innerText = `Digite um valor.`
+        }
     })
     .catch(err => console.error(`Ops, deu erro!`, err))
-      
 }
+
+
 
 //events =====================================================================================
 btnConverter.addEventListener('click', converter)
+btnClean.addEventListener('click', cleanResult)
+
+//clean functions ======================================================================
+function cleanResult() {
+    result.innerHTML = ''
+    euroField.value = ''
+}
+
+
