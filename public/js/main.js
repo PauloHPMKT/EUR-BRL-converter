333bc978377e859787fd571517aa3f2d7c3f02a8
Paulo Sérgio HPMKT <72327120+PauloHPMKT@users.noreply.github.com>
2021-11-25 10:07:11 -0300

    consumindo API de cotações EUR-BRL

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,33 @@
 //cashe selectors =============================================================================
 const euroField = document.querySelector('#euroAmount')
-const priceOfTheDay = document.querySelector('span')
+    //const priceOfTheDay = document.querySelector('span')
 const btnConverter = document.querySelector('#btnConverter')
 const btnClean = document.querySelector('#btnClean')
 const result = document.querySelector('#result')
 
 //api url Fixer.io ============================================================================
-const urlApiKey = 'http://data.fixer.io/api/latest?access_key=4db85286f1814f557cd74c6881892ffe'
+const urlApiKey = 'https://economia.awesomeapi.com.br/last/EUR-BRL'
 
 
-//new feature - priceOfTheDay.innerHTML = `Cotação do dia: ${(euroAmount).toFixed(2)}`
+// new feature - priceOfTheDay.innerHTML = `Cotação do dia: ${(euroAmount).toFixed(2)}`
 
 //fetch api function ==========================================================================
 function converter() {
     fetch(urlApiKey)
-    .then(res => res.json())
-    .then(data => {
-        const euroAmount = data.rates.BRL
-        const euro = euroField.value
-        const real = (euro * euroAmount).toFixed(2)
+        .then(res => res.json())
+        .then(data => {
+            const euroAmount = data.EURBRL.bid
+            const euro = euroField.value
+            const real = (euro * euroAmount).toFixed(2)
 
-        if(euro !== '') {
-            result.innerHTML = `Valor convertido: € ${euro} <br> Valor total: R$ ${real}`
-        } else {
-            result.innerText = `Digite um valor.`
-        }
-    })
-    .catch(err => console.error(`Ops, deu erro!`, err))
+            if (euro !== '') {
+                result.innerHTML = `Valor convertido: € ${euro} <br> Valor total: R$ ${real}`
+            } else {
+                result.innerText = `Digite um valor.`
+            }
+        })
+        .catch(err => console.error(`Ops, deu erro!`, err))
 }
-
-
 
 //events =====================================================================================
 btnConverter.addEventListener('click', converter)
@@ -40,5 +38,3 @@ function cleanResult() {
     result.innerHTML = ''
     euroField.value = ''
 }
-
-
